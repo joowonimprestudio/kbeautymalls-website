9d122a61a070dd63e1b076da1be47b14006fc25f
joowonimprestudio <joowon.im.prestudio@gmail.com>
2025-07-29 13:29:07 -0400

    Delete script_thankyou.js

--- script_thankyou.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const banner = document.querySelector('.top-banner');
-    if (window.scrollY > 0) {
-        banner.classList.add('scrolled');
-    } else {
-        banner.classList.remove('scrolled');
-    }
-});
